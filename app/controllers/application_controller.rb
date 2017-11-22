e1b30bcb6634651913555ff262a16cfb2aed5d71
mateoc10 <mateoc10@gmail.com>
2017-11-22 21:47:57 +0000

    started changes to microservice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,41 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def home
+     redirect_to ENV['URL_APP'] 
+  end
+  
+  def signup_user
+      redirect_to ENV['URL_APP'] + 'signup'
+  end
+  
+  def new_user_session
+      
+  end
+  
+  def create_user_session
+      
+  end
+  
+  def destroy_user_session
+      
+  end 
+  
+  def new_driver
+      
+  end
+  
+  def create_driver
+      
+  end
+  
+  def end_shipment
+      
+  end
+  
+  def shipment_driver
+      
+  end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,62 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # get 'shipments/new'
+  # get 'admins/confirm'
+  # get 'driver_sessions/new'
+  # get 'drivers/new'
+  # get 'sessions/new'
+  # get 'admins/new'
+  # get 'admins/confirm_driver'
+  # post 'admins/confirm_driver'
+  # get 'drivers/shipment_list'
+  # get 'drivers/shipment'
+  # post 'drivers/shipment'
+  # get 'shipments/details'
+  # get 'admins/login'
+  # get 'drivers/get_drivers_json'
+
+  root 'application#home'
+  # get 'users/new'
+  get  '/signup',  to: 'application#signup_user'
+  # get 'layout/home'
+  
+  # resources :users
+  # post '/signup',  to: 'application#signup_user'
+  # # get '/get_user_by_id' to: 'users#get_user_by_id'
+  get    '/login',   to: 'application#new_user_session'
+  # post   '/login',   to: 'application#create_user_session'
+  # delete '/logout',  to: 'application#destroy_user_session'
+  
+  # resources :drivers
+  # get '/drivers/signup', to: 'drivers#new'
+  # post '/drivers/signup', to: 'drivers#create'
+  # get 'drivers/shipment',  to: 'drivers#shipment'
+  # post 'drivers/shipment',  to: 'drivers#end_shipment'
+  # get '/drivers_session/login',   to: 'driver_sessions#new'
+  # post '/drivers_session/login',   to: 'driver_sessions#create' 
+  # post 'drivers/delivered', to: 'drivers#end_shipment'
+  # get 'drivers/get_drivers_json', to: 'drivers#get_drivers_json', format: :json
+  
+  
+  # resources :shipments
+  # get 'envios-ya-mateoc10.c9users.io:8080', to: 'shipments#new'
+  # get '/shipments/create', to: 'shipments#new'
+  # post '/shipments/create', to: 'shipments#create'
+  # post '/shipments/sendLocations', to: 'shipments#receive_locations'
+  # post '/shipment/details', to: 'shipments#details'
+  
+  # resources :admin
+  # post '/admins/login', to: 'admins#login'
+  # post 'approve', to: 'admins#approve'
+  # post 'reject', to: 'admins#reject'
+  # # get '/admins/create', to: 'admins#create'
+  # post '/admins/approve', to: 'admins#approve'
+  # post '/admins/reject', to: 'admins#reject'
+  
+  # #get '/admins/admins/create', to: 'admins#approve'
+  # #post '/reject', to: 'admins#reject'
+  # health_check_routes
+  # match '*path' => 'application#routing_error', :via => [:get]
+  
+  
 end
